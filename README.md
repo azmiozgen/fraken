0e10a5671504b954404491da0980f1a6862626ff
Azmi C. Ozgen <ozgenaz@itu.edu.tr>
2016-08-15 00:53:03 +0300

    Fix changing complex points issue for zoom option and add usage part to README.md

--- README.md
+++ README.md
@@ -8,7 +8,33 @@ Fraken = Fractal + Kraken
 
 *Release the Krakeeen!*
 
-###**TODO**
+## Usage
+
+You can see all options by:
+
+`./release.py -h`
+
+Generating default Mandelbrot fractal:
+
+`./release.py`
+
+Generating Julia fractal with *c=-0.8j*. Real and imaginary parts are given separately. '-w' shows image, '-o' saves image with given file name. e.g.
+
+`./release.py -m julia -c 0 -0.8 -w -o julia_fractal.png`
+
+Image shape can be arranged with -s option (width x height). e.g.
+
+`./release.py -m julia -c 0 -0.8 -s 1000 800 -w -o julia_fractal.png`
+
+Zoom in/out option is -z, threshold -t, iteration limit -l and coloring map -r. e.g.
+
+`./release.py -m julia -c 0 -0.8 -t 2 -l 1000 -s 800 700 -z 0.7 -r hot -w -o julia_fractal.png`
+
+Result:
+
+![julia fractal](julia_fractal.png)
+
+### **TODO**
 - Make fractals properly colored
 - Optimize operations at lists, numpy matrices, opencv etc.
 - Design fractal animations
---
+++ release.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--model", type=str, default='mandelbrot', help="Model name. 'mandelbrot' or 'julia'. default='mandelbrot'")
+parser.add_argument("-c", "--constant", type=float, default=[None, None], nargs='*', help="Constant 'c' for julia set. default=None")
+parser.add_argument("-t", "--threshold", type=int, default=2, help="Threshold value for escaping. default=2")
+parser.add_argument("-l", "--limit", type=int, default=1000, help="Limit iteration value. default=1000")
+parser.add_argument("-s", "--shape", type=int, default=[300, 200], nargs='*', help="Image shape. e.g. -s 300 200. default=300 200")
+parser.add_argument("-z", "--zoom", type=float, default=0.65, help="Zoom ratio. e.g. -z 0.65. default=0.65")
+parser.add_argument("-r", "--color", type=str, default='hot', help="Coloring map. default='hot'")
+parser.add_argument("-w", "--show", action='store_true', help="Show image")
+parser.add_argument("-o", "--output", type=str, help="Save image on given path")
+args = vars(parser.parse_args())
+model = args["model"]
+constant = complex(args["constant"][0], args["constant"][1])
+threshold = args["threshold"]
+limit = args["limit"]
+shape = args["shape"]
+zoom = args["zoom"]
+color = args["color"]
+show = args["show"]
+output = args["output"]
+
+def make_z_plane(width=300, height=200, zoom=0.60):
+	bound = round((1. / zoom), 1)
+	a = np.arange(-bound, bound, 2. * bound / width)
+	b = -np.arange(-bound, bound, 2. * bound / height)
+	z_plane = np.array([np.complex(r, i) for i in b for r in a])
+	z_plane = z_plane.reshape(height, width)
+	return z_plane
+
+def release(z_plane, model="mandelbrot", c=None, threshold=2, limit=1000):
+	image = np.zeros(z_plane.shape, dtype=np.uint8)
+	for [i, j], z in np.ndenumerate(z_plane):
+		if model == "mandelbrot":
+			c = z
+		elif model == "julia":
+			if c == None:
+				raise ValueError("c must be given!")
+		else:
+			raise ValueError("Model not found!")
+		intensity = 0
+		while (abs(z) < threshold) and (intensity < limit):
+			z = z * z + c
+			intensity += 1
+		image[i, j] = intensity
+	return image
+
+if __name__ == "__main__":
+	z_plane = make_z_plane(width=shape[0], height=shape[1], zoom=zoom)
+	img = release(z_plane, model=model, c=constant, threshold=threshold, limit=limit)
+	plt.imshow(img, cmap=color)
+	plt.xticks([])
+	plt.yticks([])
+	if output:
+		plt.savefig(output, bbox_inches='tight', pad_inches=0)
+	if show:
+		plt.show()
