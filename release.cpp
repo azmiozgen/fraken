5481f911e39f61126a95e1e7dd18087e06481060
Azmi Ozgen <azmican73@gmail.com>
2016-08-23 01:31:34 +0300

    Fix opencv libraries inconsistency.

--- release.cpp
+++ release.cpp
@@ -1,8 +1,6 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/core/utility.hpp>
-#include "opencv2/imgcodecs.hpp"
-#include <opencv2/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <complex>
 #include <string>
